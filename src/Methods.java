a7761ade2d49b778883796ac53f748de78e03252
kylielent <lentk@student.swosu.edu>
2015-11-11 09:48:37 -0600

    All three rounds are working.

--- src/Methods.java
+++ src/Methods.java
@@ -10,18 +10,37 @@ public class Methods {
 	public static void additiongameMethod() {
 		System.out.println("Inside the addition game method.");
 		int hardness = 5;
+		int score = 0;
+		int hardnesslevel = 2;
 		
 		//Set up my for loop to go through the number of rounds
 		
-		int NumberofRounds = 1;
-		for (int roundnumber =1; 
+		int NumberofRounds = 3;
+		for (int roundnumber = 1; 
 		roundnumber <= NumberofRounds;
 		roundnumber = roundnumber +1){
 			System.out.println("Inside the for loop. Round: " + roundnumber);
 			boolean isanswercorrect = getandcheckstudentanswer(hardness);
-		
+			if (isanswercorrect){
+				System.out.print("Your score is " + score + " and is now ");
+				score = score + hardness;
+				System.out.println(score + "." );
+				
+				if (roundnumber<NumberofRounds){
+					System.out.print("Your hardness was " + hardness + " and is now ");
+					if (hardness > 5){
+						hardness = hardness / hardnesslevel;
+					}
+						System.out.println(hardness + ".");
+					}
+					
+				}
 			}
-	}
+				System.out.println("The game is complete.");
+				System.out.println("Your final score was " + score);
+		}
+	
+	
 	public static boolean getandcheckstudentanswer(int hardness) {
 		System.out.println("Inside get and check student answer method.");
 		System.out.println("Add two integers together and give an integer back.");
@@ -29,7 +48,7 @@ public class Methods {
 		int number2 = (int)(Math.random () * hardness);
 		System.out.println("Add " + number1 + " and " + number2 + ".");
 		//Scanner get = new Scanner(System.in);
-		//int studentanswer = input.nextInt();
+		//studentanswer = input.nextInt();
 		Scanner get = new Scanner(System.in);
 		int studentanswer = get.nextInt();
 		if(studentanswer == (number1 + number2)){
@@ -42,5 +61,6 @@ public class Methods {
 	}
 }
 			
-			
+	
+
 
